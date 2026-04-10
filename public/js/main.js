acb76aef99564439bba3f809fa5e10c294774353
Ruben Miranda <ruben.miranda.f@gmail.com>
2026-04-09 23:43:04 -0400

    last update

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,11 @@
 // public/js/main.js
 import { initLanguage, loadLanguage } from './modules/i18n.js';
-import { initModal } from './modules/modal.js';
 import { initNavObserver, initMobileMenu } from './modules/nav.js';
 import { initCarousel } from './modules/carousel.js'; // Nuevo
 
 document.addEventListener('DOMContentLoaded', () => {
     initMobileMenu();
     initNavObserver();
-    initModal();
     initCarousel(); // Inicializamos el carrusel
 
     initLanguage(() => {
